ca5c737ec8cd4fadcd1397f673e68e1f6df8174f
Gabriel Almeida <gabriel.c94@gmail.com>
2017-07-23 21:05:18 -0300

    atualizado ast para a ultima versao por leandro

--- ast.cpp
+++ ast.cpp
@@ -3,63 +3,64 @@
 #include <string>
 #include <map>
 #include <list>
-#include "ast.hpp"
+#include "AST_Nai_Lore.hpp"
 
 using namespace std;
 
-// the constructor for node links the node to its children,
-// and stores the character representation of the operator.
+
+//TODO DUVIDA AQUI
+//Abstrações
+
+//#################################################################################################
+
+
+//CADA O EXP_NODE
+
+//Expr        = Expr BinOp Expr | UnOp Expr | '(' Expr ')' | FuncCall | DEC | ID
+
 operator_node::operator_node(exp_node *L, exp_node *R) {
     left    = L;
     right   = R;
 }
 
-exp_paren_node::exp_paren_node(exp_node *exp) {
-    expr = exp;
+//#################################################################################################
+
+//REVISAR. ESSES PRINTS ESTAO ESTRANHOS.
+//UnOp        = '-'  | '!'
+
+unary_minus_node::unary_minus_node(exp_node *L) : exp(L) {}
+
+void unary_minus_node:: print() {
+    cout << "[- ";
+    exp->print();
+    cout << "]";
 }
 
-void exp_paren_node:: print() {
-    expr->print();
-}
+unary_neg_node::unary_neg_node(exp_node *L) : exp(L) {}
 
+void unary_neg_node:: print() {
+    cout << "[! ";
+    exp->print();
+    cout << "]";
 
-
-
-number_node::number_node(float value) {
-    num = value;
-}
-
-void number_node:: print() {
-    cout << "[" << num << "]";
-}
-
-
-
-id_node::id_node(string value) : id(value) {}
-
-void id_node:: print() {
-    cout <<  "[" << id << "]";
 }
 
 
+//#################################################################################################
 
-// plus_node inherits the characteristics of node and adds its own evaluate function
-// plus_node's constructor just uses node's constructor
+
+//BinOp       = '+' | '-' | '*' | '/' | '<' | '<=' | '>' | '>=' | '==' | '!=' | '&&' | '||'
 plus_node::plus_node(exp_node *L, exp_node *R) : operator_node(L,R) {
 }
 
 void plus_node:: print() {
     cout << "[+ ";
     left->print();
-    right->print();
+    right->print();é
     cout << "]";
     
 }
 
-
-
-
-// minus_node inherits the characteristics of node and adds its own evaluate function
 minus_node::minus_node(exp_node *L, exp_node *R) : operator_node(L,R) {
 }
 
@@ -71,10 +72,6 @@ void minus_node:: print() {
     
 }
 
-
-
-
-// times_node inherits the characteristics of node and adds its own evaluate function
 times_node::times_node(exp_node *L, exp_node *R) : operator_node(L,R) {
 }
 
@@ -84,11 +81,6 @@ void times_node:: print() {
     right->print();
     cout << "]";
 }
-
-
-
-
-// divide_node inherits the characteristics of node and adds its own evaluate function
 
 divide_node::divide_node(exp_node *L, exp_node *R) : operator_node(L,R) {
 }
@@ -100,9 +92,6 @@ void divide_node:: print() {
     cout << "]";
 }
 
-
-
-
 menor_node::menor_node(exp_node *L, exp_node *R) : operator_node(L,R) {
 }
 
@@ -112,7 +101,6 @@ void menor_node:: print() {
     right->print();
     cout << "]";
 }
-
 
 menori_node::menori_node(exp_node *L, exp_node *R) : operator_node(L,R) {
 }
@@ -124,8 +112,6 @@ void menori_node:: print() {
     cout << "]";
 }
 
-
-
 maior_node::maior_node(exp_node *L, exp_node *R) : operator_node(L,R) {
 }
 
@@ -135,7 +121,6 @@ void maior_node:: print() {
     right->print();
     cout << "]";
 }
-
 
 maiori_node::maiori_node(exp_node *L, exp_node *R) : operator_node(L,R) {
 }
@@ -147,8 +132,6 @@ void maiori_node:: print() {
     cout << "]";
 }
 
-
-
 iigual_node::iigual_node(exp_node *L, exp_node *R) : operator_node(L,R) {
 }
 
@@ -158,9 +141,6 @@ void iigual_node:: print() {
     right->print();
     cout << "]";
 }
-
-
-
 
 dif_node::dif_node(exp_node *L, exp_node *R) : operator_node(L,R) {
 }
@@ -172,7 +152,6 @@ void dif_node:: print() {
     cout << "]";
 }
 
-
 e_node::e_node(exp_node *L, exp_node *R) : operator_node(L,R) {
 }
 
@@ -182,8 +161,6 @@ void e_node:: print() {
     right->print();
     cout << "]";
 }
-
-
 
 ou_node::ou_node(exp_node *L, exp_node *R) : operator_node(L,R) {
 }
@@ -196,49 +173,46 @@ void ou_node:: print() {
 }
 
 
+//#################################################################################################
 
 
-// unary_minus_node inherits the characteristics of node and adds its own evaluate function
-unary_minus_node::unary_minus_node(exp_node *L) : exp(L) {}
+//Expr        = Expr BinOp Expr | UnOp Expr | '(' Expr ')' | FuncCall | DEC | ID
 
-void unary_minus_node:: print() {
-    cout << "-";
-    exp->print();
+//DUVIDA Devo colocar os cochete aqui, ou o bison cuida da precedencia?
+exp_paren_node::exp_paren_node(exp_node *exp) {
+    expr = exp;
+}
+
+void exp_paren_node:: print() {
+    expr->print();
+}
+
+dec_node::dec_node(float value) {num = value;}
+
+void dec_node:: print() {
+    cout << "[" << num << "]";
+}
+
+id_node::id_node(string value) : id(value) {}
+
+void id_node:: print() {
+    cout <<  "[" << id << "]";
 }
 
 
 
-
-
-unary_neg_node::unary_neg_node(exp_node *L) : exp(L) {}
-
-void unary_neg_node:: print() {
-    cout << "!";
-    exp->print();
-}
-
-
-decvarst_node::decvarst_node(string name, exp_node *expression)
-: id(name), exp(expression) {}
-
-void decvarst_node::print() {
-    cout << "[decvar[" << id <<"]" ;
-    if(exp != NULL){
-    exp->print();
-    }
-    cout  <<"]";
-}
+//#################################################################################################
 
 
 
-
+//ArgList     = Expr (',' Expr)*
 arglist_node::arglist_node(list<exp_node *> *argumentos){
     args = argumentos;
 }
 
 void arglist_node::print() {
     list<exp_node *>::iterator argsIter;
-
+    
     cout <<  "[arglist";
     
     for (argsIter = args->begin(); argsIter != args->end();
@@ -249,6 +223,56 @@ void arglist_node::print() {
     
     cout <<"]\n";
 }
+
+
+//#################################################################################################
+
+
+
+//FuncCall    = ID '(' ArgList? ')'
+funccall_node::funccall_node(string name, arglist_node *argumento) : id(name), arg(argumento) {}
+
+void funccall_node::print() {
+    list<arglist_node *>::iterator argsIter;
+    
+    cout << "[funccall[" << id <<"]" ;
+    arg->print();
+    
+    
+    cout  <<"]";
+}
+
+
+//#################################################################################################
+
+
+//Assign      = ID '=' Expr
+assignment_stmt::assignment_stmt(string name, exp_node *expression): id(name), exp(expression) {}
+
+void assignment_stmt::print() {
+    cout << "[assign [" << id <<"]" ;
+    exp->print();
+    cout  <<"]";
+}
+
+
+//#################################################################################################
+
+
+//DecVar      = let ID ('=' Expr)? ';'
+decvarst_node::decvarst_node(string name, exp_node *expression)
+: id(name), exp(expression) {}
+
+void decvarst_node::print() {
+    cout << "[decvar[" << id <<"]" ;
+    if(exp != NULL){
+        exp->print();
+    }
+    cout  <<"]";
+}
+
+
+//#################################################################################################
 
 
 block::block(list<decvarst_node *> *declarations, list<statement *> *statments)
@@ -262,7 +286,7 @@ void block::print() {
     cout << "[block" ;
     if(decvars!=NULL){
         for (decVarIter = decvars->begin(); decVarIter != decvars->end();
-            decVarIter++) {
+             decVarIter++) {
             (*decVarIter)->print();
         }
     }
@@ -270,7 +294,7 @@ void block::print() {
     
     if(stms!=NULL){
         for (statmantsIter = stms->begin(); statmantsIter != stms->end  ();
-         statmantsIter++) {
+             statmantsIter++) {
             (*statmantsIter)->print();
         }
     }
@@ -278,6 +302,88 @@ void block::print() {
     
 }
 
+
+//#################################################################################################
+
+
+//Stmt        = Assign ';' | FuncCall ';' | if '(' Expr ')' Block (else Block)? | while '(' Expr ')' Block | return Expr? ';' | break ';' | continue ';'
+
+//funccall_stmt::funccall_stmt(exp_node *expression)
+//{
+//    exp = expression;
+//}
+//
+//void funccall_stmt::print() {
+//    exp->print();
+//}
+
+func_call_stmt::func_call_stmt(string identificador, exp_node *expression)
+:  exp(expression) {
+    id=identificador;
+}
+
+void func_call_stmt::print() {
+    cout << "[funccall[" << id <<"]" ;
+    exp->print();
+    cout  <<"]";
+}
+
+if_else_stmt::if_else_stmt(exp_node *expression, block *if_block, block *else_block) {
+    exp = expression;
+    _if = if_block;
+    _else = else_block;
+}
+
+void if_else_stmt::print() {
+    cout << "[if";
+    exp->print();
+    
+    
+    _if->print();
+    _else->print();
+}
+
+while_stmt::while_stmt(exp_node *expression, block *whileBLK) {
+    exp = expression;
+    while_block = whileBLK;
+}
+
+void while_stmt::print() {
+    cout << "[while";
+    exp->print();
+    
+    
+    while_block->print();
+}
+
+return_stmt::return_stmt(exp_node *expression)
+: exp(expression) {}
+
+void return_stmt::print() {
+    cout << "[return " ;
+    exp->print();
+    cout  <<"]";
+}
+
+
+break_stmt::break_stmt(string value) : id(value) {}
+
+void break_stmt:: print() {
+    cout <<  "[" << id << "]";
+}
+
+
+continue_stmt::continue_stmt(string value) : id(value) {}
+
+void continue_stmt:: print() {
+    cout <<  "[" << id << "]";
+}
+
+
+//#################################################################################################
+
+
+//ParamList   = ID (',' ID)*
 paramlist::paramlist(list<id_node *> *nodes_ids)
 {
     ids =nodes_ids;
@@ -298,8 +404,10 @@ void paramlist::print() {
 }
 
 
+//#################################################################################################
 
 
+//DecFunc     = def ID '(' ParamList? ')' Block
 decfunc::decfunc(string identifier,paramlist *listParams,block *functionBlock)
 {
     id=identifier;
@@ -317,117 +425,10 @@ void decfunc::print() {
 }
 
 
+//#################################################################################################
 
 
-
-func_call_stmt::func_call_stmt(string identificador, exp_node *expression)
-:  exp(expression) {
-    id=identificador;
-}
-
-
-
-
-funccall_node::funccall_node(string name, arglist_node *argumento)
-: id(name), arg(argumento) {}
-
-void funccall_node::print() {
-    list<arglist_node *>::iterator argsIter;
-
-    cout << "[funccall[" << id <<"]" ;
-    arg->print();
-    
-
-    cout  <<"]";
-}
-
-
-if_else_stmt::if_else_stmt(exp_node *expression, block *if_block, block *else_block) {
-    exp = expression;
-    _if = if_block;
-    _else = else_block;
-}
-
-void if_else_stmt::print() {
-    cout << "[if";
-    exp->print();
-
-    
-    _if->print();
-    _else->print();
-}
-
-while_stmt::while_stmt(exp_node *expression, block *whileBLK) {
-    exp = expression;
-    while_block = whileBLK;
-}
-
-void while_stmt::print() {
-    cout << "[while";
-    exp->print();
-    
-    
-    while_block->print();
-}
-
-
-void func_call_stmt::print() {
-    cout << "[funccall[" << id <<"]" ;
-    exp->print();
-    cout  <<"]";
-}
-
-
-
-
-funccall_stmt::funccall_stmt(exp_node *expression)
-{
-    exp = expression;
-}
-
-void funccall_stmt::print() {
-    exp->print();
-}
-
-break_stmt::break_stmt(string value) : id(value) {}
-
-void break_stmt:: print() {
-    cout <<  "[" << id << "]";
-}
-
-continue_stmt::continue_stmt(string value) : id(value) {}
-
-void continue_stmt:: print() {
-    cout <<  "[" << id << "]";
-}
-
-
-return_stmt::return_stmt(exp_node *expression)
-: exp(expression) {}
-
-void return_stmt::print() {
-    cout << "[return " ;
-    exp->print();
-    cout  <<"]";
-}
-
-assignment_stmt::assignment_stmt(string name, exp_node *expression)
-: id(name), exp(expression) {}
-
-void assignment_stmt::print() {
-    cout << "[assign[" << id <<"]" ;
-    exp->print();
-    cout  <<"]";
-}
-
-
-
-print_stmt::print_stmt (string name) : id(name) {}
-
-
-
-
-
+//Program     = (DecVar | DecFunc)*
 pgm::pgm(list<decvarst *> *stmtlist) {
     stmts = stmtlist;
 }
@@ -455,13 +456,28 @@ void pgm::print() {
 
 
 
+print_stmt::print_stmt (string name) : id(name) {}
 
 map<string, float> idTable;
 
 
+
+
+
+
+
+
+
+
+//#################################################################################################
+//#################################################################################################
+//###################         Testes                                ###############################
+//#################################################################################################
+//#################################################################################################
+
 void test1(){
     exp_node *op1 = new id_node("a");
-    exp_node *op2 = new number_node(2);
+    exp_node *op2 = new dec_node(2);
     list<exp_node *> *args = new list<exp_node *>;
     args->push_back(op1);
     args->push_back(op2);
@@ -473,7 +489,7 @@ void test1(){
     
     
     list<decvarst_node *> *decvars = new list<decvarst_node *>;
-    exp_node *op3 = new plus_node(new number_node(3),new number_node(5));
+    exp_node *op3 = new plus_node(new dec_node(3),new dec_node(5));
     exp_node *op4 = new id_node("b");
     
     
@@ -500,7 +516,7 @@ void test1(){
 
 void test2(){
     exp_node *op1 = new id_node("a");
-    exp_node *op2 = new number_node(2);
+    exp_node *op2 = new dec_node(2);
     list<exp_node *> *args = new list<exp_node *>;
     args->push_back(op1);
     args->push_back(op2);
@@ -528,10 +544,10 @@ void test2(){
     list<decvarst_node *> *decvars2 = new list<decvarst_node *>;
     decvars2->push_back(new decvarst_node("a",NULL));
     list<statement *> *stmts2 = new list<statement *>;
-    stmts2->push_back(new assignment_stmt("a", new number_node(3)));
+    stmts2->push_back(new assignment_stmt("a", new dec_node(3)));
     
     exp_node *op3 = new id_node("a");
-    exp_node *op4 = new number_node(2);
+    exp_node *op4 = new dec_node(2);
     list<exp_node *> *args2 = new list<exp_node *>;
     args2->push_back(op3);
     args2->push_back(op4);
@@ -553,7 +569,7 @@ void test2(){
     decfuncs->push_back(funcao);
     decfuncs->push_back(funcao2);
 
-    //funcao->print();
+    funcao->print();
     
     
     
@@ -563,12 +579,12 @@ void test2(){
     pgm *program = new pgm(decfuncs);
     program->print();
     
+
+    
+    
+    
 }
 
 int main(){
-    
-    //test1();
-    //test2();
-
-    
+     
 }
--- ast.hpp
+++ ast.hpp
@@ -6,34 +6,30 @@
 
 using namespace std;
 
+//TODO DUVIDA AQUI
+//Abstrações
 class exp_node {
 public:
     float num;
-    
-    // print function for pretty printing an expression
     virtual void print() = 0;
     
-    // evaluation function for a leaf, replaced for interior nodes
 };
 
 class operator_node : public exp_node {
 public:
     exp_node *left;
     exp_node *right;
-    
-    // the constructor for node links the node to its children,
-    // and stores the character representation of the operator.
+
     operator_node(exp_node *L, exp_node *R);
 };
 
-class number_node : public exp_node {
-    
-public:
-    number_node(float value);
-    void print();
-};
 
+//#################################################################################################
+
+
+//UnOp        = '-'  | '!'
 class unary_minus_node : public exp_node {
+    //Acho que nao precisa
 protected:
     exp_node *exp;
 public:
@@ -41,6 +37,101 @@ public:
     void print();
 };
 
+
+class unary_neg_node : public exp_node {
+    //Acho que nao precisa
+protected:
+    exp_node *exp;
+public:
+    unary_neg_node(exp_node *exp);
+    void print();
+};
+
+
+//#################################################################################################
+
+
+//BinOp       = '+' | '-' | '*' | '/' | '<' | '<=' | '>' | '>=' | '==' | '!=' | '&&' | '||'
+class plus_node : public operator_node {
+public:
+    plus_node(exp_node *L, exp_node *R);
+    void print();
+};
+
+
+class minus_node : public operator_node {
+public:
+    minus_node(exp_node *L, exp_node *R);
+    void print();
+};
+
+
+class times_node : public operator_node {
+public:
+    times_node(exp_node *L, exp_node *R);
+    void print();
+};
+
+
+class divide_node : public operator_node {
+public:
+    divide_node(exp_node *L, exp_node *R);
+    void print();
+};
+
+class menor_node : public operator_node {
+public:
+    menor_node(exp_node *L, exp_node *R);
+    void print();
+};
+
+class menori_node : public operator_node {
+public:
+    menori_node(exp_node *L, exp_node *R);
+    void print();
+};
+
+class maior_node : public operator_node {
+public:
+    maior_node(exp_node *L, exp_node *R);
+    void print();
+};
+
+class maiori_node : public operator_node {
+public:
+    maiori_node(exp_node *L, exp_node *R);
+    void print();
+};
+
+class iigual_node : public operator_node {
+public:
+    iigual_node(exp_node *L, exp_node *R);
+    void print();
+};
+
+class dif_node : public operator_node {
+public:
+    dif_node(exp_node *L, exp_node *R);
+    void print();
+};
+
+class e_node : public operator_node {
+public:
+    e_node(exp_node *L, exp_node *R);
+    void print();
+};
+
+class ou_node : public operator_node {
+public:
+    ou_node(exp_node *L, exp_node *R);
+    void print();
+};
+
+
+//#################################################################################################
+
+
+//Expr        = Expr BinOp Expr | UnOp Expr | '(' Expr ')' | FuncCall | DEC | ID
 class exp_paren_node : public exp_node {
 protected:
     exp_node *expr;
@@ -49,82 +140,29 @@ public:
     void print();
 };
 
-
-class unary_neg_node : public exp_node {
+class dec_node : public exp_node {
 protected:
-    exp_node *exp;
+    float dec;
 public:
-    unary_neg_node(exp_node *exp);
+    dec_node(float value);
     void print();
 };
 
 class id_node : public exp_node {
 protected:
     string id;
-    
 public:
     id_node(string value);
     void print();
 };
 
 
-// plus_node inherits the characteristics of node and adds its own evaluate function
-class plus_node : public operator_node {
-public:
-    
-    // plus_node's constructor just uses node's constructor
-    plus_node(exp_node *L, exp_node *R);
-    void print();
-};
+
+//#################################################################################################
 
 
-// minus_node inherits the characteristics of node and adds its own evaluate function
-class minus_node : public operator_node {
-public:
-    
-    minus_node(exp_node *L, exp_node *R);
-    void print();
-};
 
-
-// times_node inherits the characteristics of node and adds its own evaluate function
-class times_node : public operator_node {
-public:
-    
-    times_node(exp_node *L, exp_node *R);
-    void print();
-};
-
-
-// divide_node inherits the characteristics of node and adds its own evaluate function
-class divide_node : public operator_node {
-public:
-    
-    divide_node(exp_node *L, exp_node *R);
-    void print();
-};
-
-class menor_node : public operator_node {
-public:
-    
-    menor_node(exp_node *L, exp_node *R);
-    void print();
-};
-
-class menori_node : public operator_node {
-public:
-    
-    menori_node(exp_node *L, exp_node *R);
-    void print();
-};
-
-class maiori_node : public operator_node {
-public:
-    
-    maiori_node(exp_node *L, exp_node *R);
-    void print();
-};
-
+//ArgList     = Expr (',' Expr)*
 class arglist_node : public exp_node {
 protected:
     list<exp_node *> *args;
@@ -134,6 +172,12 @@ public:
     
 };
 
+
+//#################################################################################################
+
+
+
+//FuncCall    = ID '(' ArgList? ')'
 class funccall_node : public exp_node {
 protected:
     string id;
@@ -144,42 +188,11 @@ public:
 };
 
 
-class maior_node : public operator_node {
-public:
-    
-    maior_node(exp_node *L, exp_node *R);
-    void print();
-};
-
-class iigual_node : public operator_node {
-public:
-    
-    iigual_node(exp_node *L, exp_node *R);
-    void print();
-};
-
-class dif_node : public operator_node {
-public:
-    
-    dif_node(exp_node *L, exp_node *R);
-    void print();
-};
-
-class e_node : public operator_node {
-public:
-    
-    e_node(exp_node *L, exp_node *R);
-    void print();
-};
-
-class ou_node : public operator_node {
-public:
-    
-    ou_node(exp_node *L, exp_node *R);
-    void print();
-};
+//#################################################################################################
 
 
+
+//DecVar      = let ID ('=' Expr)? ';'
 class decvarst {
 public:
     virtual void print() {}
@@ -194,28 +207,20 @@ public:
     void print();
 };
 
+//#################################################################################################
+
+
 class statement {
 public:
     virtual void print() {}
 };
 
-class assignment_stmt : public statement {
-protected:
-    string id;
-    exp_node *exp;
-public:
-    assignment_stmt(string id,exp_node *expression);
-    void print();
-};
 
-class funccall_stmt : public statement {
-protected:
-    exp_node *exp;
-public:
-    funccall_stmt(exp_node *expression);
-    void print();
-};
 
+//#################################################################################################
+
+
+//Block       = '{' DecVar* Stmt* '}'
 class block {
 protected:
     list<decvarst_node *> *decvars;
@@ -227,28 +232,35 @@ public:
     
 };
 
-class paramlist {
-protected:
-    list<id_node *> *ids;
-    
-public:
-    paramlist(list<id_node *> *ids);
-    void print();
-    
-};
+//Stmt        = Assign ';' | FuncCall ';' | if '(' Expr ')' Block (else Block)? | while '(' Expr ')' Block | return Expr? ';' | break ';' | continue ';'
 
-class decfunc : public decvarst {
+
+class assignment_stmt : public statement {
 protected:
     string id;
-    paramlist *params;
-    block *funcBlock;
-    
+    exp_node *exp;
 public:
-    decfunc(string id,paramlist *params,block *funcBlock);
+    assignment_stmt(string id,exp_node *expression);
     void print();
-    
 };
 
+
+class func_call_stmt : public statement {
+protected:
+    string id;
+    exp_node *exp;
+public:
+    func_call_stmt(string id,exp_node *expression);
+    void print();
+};
+
+//class funccall_stmt : public statement {
+//protected:
+//    exp_node *exp;
+//public:
+//    funccall_stmt(exp_node *expression);
+//    void print();
+//};
 
 class if_else_stmt : public statement {
 protected:
@@ -269,15 +281,13 @@ public:
     void print();
 };
 
-class func_call_stmt : public statement {
+class return_stmt : public statement {
 protected:
-    string id;
     exp_node *exp;
 public:
-    func_call_stmt(string id,exp_node *expression);
+    return_stmt(exp_node *expression);
     void print();
 };
-
 
 class break_stmt : public statement {
 protected:
@@ -288,6 +298,7 @@ public:
     void print();
 };
 
+
 class continue_stmt : public statement {
 protected:
     string id;
@@ -297,12 +308,50 @@ public:
     void print();
 };
 
-class return_stmt : public statement {
+
+//#################################################################################################
+
+
+//ParamList   = ID (',' ID)*
+class paramlist {
 protected:
-    exp_node *exp;
+    list<id_node *> *ids;
+    
 public:
-    return_stmt(exp_node *expression);
+    paramlist(list<id_node *> *ids);
     void print();
+    
+};
+
+
+//#################################################################################################
+
+
+//DecFunc     = def ID '(' ParamList? ')' Block
+class decfunc : public decvarst {
+protected:
+    string id;
+    paramlist *params;
+    block *funcBlock;
+    
+public:
+    decfunc(string id,paramlist *params,block *funcBlock);
+    void print();
+    
+};
+
+
+//#################################################################################################
+
+
+//Program     = (DecVar | DecFunc)*
+class pgm {
+protected:
+    list<decvarst *> *stmts;
+public:
+    pgm(list<decvarst *> *stmtlist);
+    void print();
+    
 };
 
 
@@ -314,18 +363,6 @@ public:
 };
 
 
-
-
-class pgm {
-protected:
-    list<decvarst *> *stmts;
-public:
-    pgm(list<decvarst *> *stmtlist);
-    void print();
-    
-};
-
-
-// the object at the base of our tree
 extern map<string, float> idTable;
 extern pgm *root;
+
